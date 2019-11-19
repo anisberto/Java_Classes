8b15d2acde383ca44e4e2e25ccd7554c67fa93a0
Anisberto Reis <anisbertoos@gmail.com>
2019-11-18 21:10:12 -0300

    Aula Teste Java Classes

--- src/orientacao/Conta.java
+++ src/orientacao/Conta.java
@@ -1,5 +1,6 @@
 package orientacao;
 
+import java.util.Random;
 import java.util.Scanner;
 import orientacao.controller.ClasseConta;
 
@@ -7,41 +8,58 @@ public class Conta {
 
     public static void main(String[] ars) {
         Scanner input = new Scanner(System.in);
-        String tipoConta, donoConta,saldo;
-        int numeroConta,entrar,status;
+        String tipoConta, donoConta, sobreNome;
+        int saldo, entrar, status;
         boolean fecharConta;
-        double deposito,saque;
-        
-        System.out.println("Ola seja bem vindo ao Banco King!\n\nDeseja abrir sua conta? 0 - SIM || 1 - NÃO\n");
+        double deposito, saque;
+
+        System.out.println("\tOla seja bem vindo ao Banco King!\n\nDeseja abrir sua conta? 0 - SIM || 1 - NÃO\n");
         entrar = input.nextInt();
-        
-        while(entrar == 0){
+
+        while (entrar == 0) {
             System.out.println("Informe o nome do correntista: ");
             donoConta = input.next();
+            input.next();
+
             ClasseConta correntista = new ClasseConta();
-            
-            System.out.println("Legal! É otimo telo aqui!\nQual o tipo da conta? CC - pessoa fisica ou CP - pessoa juridica");
-            tipoConta = input.nextLine();
+            correntista.setNumConta(sorteia(1, 10000));
+
+            System.out.println("\nLegal! É otimo telo aqui!\nQual o tipo da conta? CC - pessoa fisica ou CP - pessoa juridica\n");
+            tipoConta = input.next();
             correntista.setdonoConta(donoConta);
             correntista.abrirConta(tipoConta);
-            
-            System.out.println("Deseja vezualizar o extrato inicial da conta: 3 - SIM || 4 - NÃO");
+
+            System.out.println("\nDeseja vezualizar o extrato inicial da conta: 3 - SIM || 4 - NÃO");
             status = input.nextInt();
-            if(status == 3){
+
+            if (status == 3) {
                 correntista.estadoAtual();
-            }else{
-                System.out.println("Sr(a)." + donoConta + " Sua conta foi aberto com sucesso!");
+            } else {
+                System.out.println("Sr(a)." + donoConta + " Sua conta foi aberto com sucesso!\n");
             }
-            
-            System.out.println("Deseja sair? 0 - SIM || 1 - NÃO");
+
+            System.out.println("Deseja sair? 0 - NÃO || 1 - SIM");
             entrar = input.nextInt();
-            if(entrar != 0 ){
-                System.out.println("Deseja ver seu saldo? SIM || NÃO");
-                saldo = input.next();
-                if(saldo == "SIM" || saldo == "sim"){
-                    System.out.println("Sr(a). " + donoConta + " Seu saldo é " + correntista.getSaldo());
+
+            if (entrar != 1) {
+                if (entrar != 0) {
+                    System.out.println("Deseja ver seu saldo? 6 - SIM || 7 - NÃO");
+                    saldo = input.nextInt();
+
+                    if (saldo == 6) {
+                        System.out.println("Sr(a). " + donoConta + " Seu saldo é R$ " + correntista.getSaldo());
+                    } else {
+                        entrar = 0;
+                    }
                 }
+            }else{
+                entrar = 1;
             }
-        }  
+        }
+    }
+
+    public static int sorteia(int limiteInferior, int limiteSuperior) {
+        Random rd = new Random();
+        return rd.nextInt(limiteSuperior - limiteInferior + 1) + limiteInferior;
     }
 }
